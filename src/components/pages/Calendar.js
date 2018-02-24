54413b5a83411d86f08177bec66850b13588cb4e
cworf <colin.worf@gmail.com>
2018-02-24 10:58:59 -0800

    add react-big-calendar

--- src/components/pages/Calendar.js
+++ src/components/pages/Calendar.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import events from '../../assets/data/events.js';
+
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
 function Calendar(props){
 
   return (
     <div>
-
+      <BigCalendar
+        selectable
+        events={events}
+        defaultView="week"
+        scrollToTime={new Date(1970, 1, 1, 6)}
+        defaultDate={new Date(2015, 3, 12)}
+        onSelectEvent={event => alert(event.title)}
+        onSelectSlot={slotInfo =>
+          alert(
+            `selected slot:
+            start ${slotInfo.start.toLocaleString()}
+            end: ${slotInfo.end.toLocaleString()}
+            action: ${slotInfo.action}`
+          )
+        }
+      />
     </div>
   );
 }
