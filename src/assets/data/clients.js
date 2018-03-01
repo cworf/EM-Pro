be89ceb32f7959edb38724b28dd6e25e2ab68d6e
Guest User <Guest@Epicodus-1A.local>
2018-03-01 11:29:13 -0800

    create dummy client data

---
+++ src/assets/data/clients.js
@@ -0,0 +1,653 @@
+export const clients = {
+  "pNjdFyiwN8": {
+    "billing_info": {
+      "address": "931 Ocean Parkway, Stagecoach, California, 2482",
+      "phone": "+1 (833) 444-3755",
+      "email": "gordon.vang@undefined.name",
+      "liaison": {
+        "last": "Vang",
+        "first": "Gordon"
+      }
+    },
+    "address": "898 Matthews Court, Fairlee, South Carolina, 2084",
+    "phone": "+1 (905) 419-2467",
+    "email": "jocelyn.simpson@nikuda.io",
+    "website": "https://nikuda.ca",
+    "company": "NIKUDA",
+    "name": {
+      "last": "Simpson",
+      "first": "Jocelyn"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "fQyvOdhktq": {
+    "billing_info": {
+      "address": "684 Remsen Avenue, Nanafalia, Marshall Islands, 3409",
+      "phone": "+1 (890) 484-2797",
+      "email": "myers.hess@undefined.biz",
+      "liaison": {
+        "last": "Hess",
+        "first": "Myers"
+      }
+    },
+    "address": "467 Willmohr Street, Cavalero, Massachusetts, 8762",
+    "phone": "+1 (937) 443-2551",
+    "email": "gloria.bryant@valpreal.tv",
+    "website": "https://valpreal.me",
+    "company": "VALPREAL",
+    "name": {
+      "last": "Bryant",
+      "first": "Gloria"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "VFafoFGHPC": {
+    "billing_info": {
+      "address": "940 Dobbin Street, Roosevelt, Wyoming, 3606",
+      "phone": "+1 (848) 463-3015",
+      "email": "chapman.christensen@undefined.net",
+      "liaison": {
+        "last": "Christensen",
+        "first": "Chapman"
+      }
+    },
+    "address": "261 Hill Street, Albany, Alabama, 3773",
+    "phone": "+1 (996) 434-2676",
+    "email": "cleveland.moses@perkle.co.uk",
+    "website": "https://perkle.info",
+    "company": "PERKLE",
+    "name": {
+      "last": "Moses",
+      "first": "Cleveland"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "OOHYG82dDn": {
+    "billing_info": {
+      "address": "518 Rogers Avenue, Kylertown, North Carolina, 7151",
+      "phone": "+1 (989) 507-3502",
+      "email": "mccormick.phillips@undefined.org",
+      "liaison": {
+        "last": "Phillips",
+        "first": "Mccormick"
+      }
+    },
+    "address": "997 Meadow Street, Windsor, Rhode Island, 9596",
+    "phone": "+1 (934) 429-2441",
+    "email": "catalina.frederick@omatom.us",
+    "website": "https://omatom.biz",
+    "company": "OMATOM",
+    "name": {
+      "last": "Frederick",
+      "first": "Catalina"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "BuNTziFgeZ": {
+    "billing_info": {
+      "address": "329 Johnson Avenue, Vaughn, New Hampshire, 9255",
+      "phone": "+1 (804) 483-2138",
+      "email": "mcneil.clarke@undefined.name",
+      "liaison": {
+        "last": "Clarke",
+        "first": "Mcneil"
+      }
+    },
+    "address": "540 Reeve Place, Iberia, Missouri, 5207",
+    "phone": "+1 (844) 469-2554",
+    "email": "perkins.hooper@zanymax.io",
+    "website": "https://zanymax.ca",
+    "company": "ZANYMAX",
+    "name": {
+      "last": "Hooper",
+      "first": "Perkins"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "gmWSlpeNKP": {
+    "billing_info": {
+      "address": "760 Broadway , Hobucken, Oregon, 155",
+      "phone": "+1 (800) 495-2072",
+      "email": "barry.gross@undefined.biz",
+      "liaison": {
+        "last": "Gross",
+        "first": "Barry"
+      }
+    },
+    "address": "663 Rutledge Street, Oretta, Maine, 6871",
+    "phone": "+1 (810) 481-2574",
+    "email": "salinas.cervantes@equicom.tv",
+    "website": "https://equicom.me",
+    "company": "EQUICOM",
+    "name": {
+      "last": "Cervantes",
+      "first": "Salinas"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "ZelJVzTTuo": {
+    "billing_info": {
+      "address": "306 Metrotech Courtr, Rodman, Maryland, 335",
+      "phone": "+1 (952) 436-3545",
+      "email": "hays.mack@undefined.net",
+      "liaison": {
+        "last": "Mack",
+        "first": "Hays"
+      }
+    },
+    "address": "412 Havemeyer Street, Blanco, Mississippi, 736",
+    "phone": "+1 (819) 510-2931",
+    "email": "debora.freeman@fibrodyne.co.uk",
+    "website": "https://fibrodyne.info",
+    "company": "FIBRODYNE",
+    "name": {
+      "last": "Freeman",
+      "first": "Debora"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "5oOl8AUQO6": {
+    "billing_info": {
+      "address": "639 Taaffe Place, Mapletown, New Mexico, 776",
+      "phone": "+1 (962) 413-3876",
+      "email": "lucas.bates@undefined.org",
+      "liaison": {
+        "last": "Bates",
+        "first": "Lucas"
+      }
+    },
+    "address": "805 Bank Street, Stewartville, Arkansas, 6561",
+    "phone": "+1 (834) 566-2072",
+    "email": "brandy.valenzuela@earwax.us",
+    "website": "https://earwax.biz",
+    "company": "EARWAX",
+    "name": {
+      "last": "Valenzuela",
+      "first": "Brandy"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "TfeMASkxby": {
+    "billing_info": {
+      "address": "823 Hancock Street, Haena, Vermont, 5168",
+      "phone": "+1 (866) 505-3128",
+      "email": "kellie.wright@undefined.name",
+      "liaison": {
+        "last": "Wright",
+        "first": "Kellie"
+      }
+    },
+    "address": "330 Hooper Street, Riegelwood, Illinois, 1166",
+    "phone": "+1 (943) 557-2489",
+    "email": "sims.dickerson@xerex.io",
+    "website": "https://xerex.ca",
+    "company": "XEREX",
+    "name": {
+      "last": "Dickerson",
+      "first": "Sims"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "LMuBWbuiKJ": {
+    "billing_info": {
+      "address": "254 Duryea Place, Ellerslie, New Jersey, 1786",
+      "phone": "+1 (939) 459-2326",
+      "email": "bowen.rosales@undefined.biz",
+      "liaison": {
+        "last": "Rosales",
+        "first": "Bowen"
+      }
+    },
+    "address": "392 Moore Street, Lisco, American Samoa, 537",
+    "phone": "+1 (904) 482-2343",
+    "email": "valdez.william@sustenza.tv",
+    "website": "https://sustenza.me",
+    "company": "SUSTENZA",
+    "name": {
+      "last": "William",
+      "first": "Valdez"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "rHbYjrKslk": {
+    "billing_info": {
+      "address": "987 Garnet Street, Harleigh, Wisconsin, 3588",
+      "phone": "+1 (982) 465-3973",
+      "email": "adriana.vargas@undefined.net",
+      "liaison": {
+        "last": "Vargas",
+        "first": "Adriana"
+      }
+    },
+    "address": "614 Ruby Street, Fowlerville, Louisiana, 3535",
+    "phone": "+1 (910) 585-3728",
+    "email": "coleman.shaw@eschoir.co.uk",
+    "website": "https://eschoir.info",
+    "company": "ESCHOIR",
+    "name": {
+      "last": "Shaw",
+      "first": "Coleman"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "hDvK9wCOiT": {
+    "billing_info": {
+      "address": "491 Evans Street, Skyland, Oklahoma, 2182",
+      "phone": "+1 (998) 598-2624",
+      "email": "irwin.mayer@undefined.org",
+      "liaison": {
+        "last": "Mayer",
+        "first": "Irwin"
+      }
+    },
+    "address": "481 Ditmars Street, Allentown, Northern Mariana Islands, 2232",
+    "phone": "+1 (923) 517-2062",
+    "email": "kelly.adams@navir.us",
+    "website": "https://navir.biz",
+    "company": "NAVIR",
+    "name": {
+      "last": "Adams",
+      "first": "Kelly"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "sKrvxQ3Mdi": {
+    "billing_info": {
+      "address": "560 Holt Court, Cetronia, Michigan, 7054",
+      "phone": "+1 (815) 512-3644",
+      "email": "mcgowan.wallace@undefined.name",
+      "liaison": {
+        "last": "Wallace",
+        "first": "Mcgowan"
+      }
+    },
+    "address": "278 Navy Street, Crumpler, Minnesota, 2274",
+    "phone": "+1 (997) 497-2347",
+    "email": "sanford.duran@geekus.io",
+    "website": "https://geekus.ca",
+    "company": "GEEKUS",
+    "name": {
+      "last": "Duran",
+      "first": "Sanford"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "nO1VzHw8PM": {
+    "billing_info": {
+      "address": "425 Oxford Street, Camas, North Dakota, 1053",
+      "phone": "+1 (826) 485-2273",
+      "email": "joseph.duke@undefined.biz",
+      "liaison": {
+        "last": "Duke",
+        "first": "Joseph"
+      }
+    },
+    "address": "293 Prospect Avenue, Beechmont, Virgin Islands, 4779",
+    "phone": "+1 (932) 531-3231",
+    "email": "kaufman.clemons@verton.tv",
+    "website": "https://verton.me",
+    "company": "VERTON",
+    "name": {
+      "last": "Clemons",
+      "first": "Kaufman"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "st3FBX6dT9": {
+    "billing_info": {
+      "address": "879 Thames Street, Bentley, Connecticut, 6746",
+      "phone": "+1 (853) 426-3958",
+      "email": "cameron.henson@undefined.net",
+      "liaison": {
+        "last": "Henson",
+        "first": "Cameron"
+      }
+    },
+    "address": "520 Driggs Avenue, Keyport, Ohio, 8459",
+    "phone": "+1 (832) 460-3370",
+    "email": "mia.james@quonata.co.uk",
+    "website": "https://quonata.info",
+    "company": "QUONATA",
+    "name": {
+      "last": "James",
+      "first": "Mia"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "hHsTCKGe9V": {
+    "billing_info": {
+      "address": "869 Elmwood Avenue, Lynn, Indiana, 6945",
+      "phone": "+1 (857) 445-3099",
+      "email": "bowers.caldwell@undefined.org",
+      "liaison": {
+        "last": "Caldwell",
+        "first": "Bowers"
+      }
+    },
+    "address": "316 Eldert Lane, Cedarville, Pennsylvania, 3588",
+    "phone": "+1 (827) 594-3461",
+    "email": "hoffman.bowers@apexia.us",
+    "website": "https://apexia.biz",
+    "company": "APEXIA",
+    "name": {
+      "last": "Bowers",
+      "first": "Hoffman"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "kfIUdc2ai8": {
+    "billing_info": {
+      "address": "240 Elizabeth Place, Roland, Georgia, 2774",
+      "phone": "+1 (961) 571-3087",
+      "email": "alice.roberts@undefined.name",
+      "liaison": {
+        "last": "Roberts",
+        "first": "Alice"
+      }
+    },
+    "address": "350 Homecrest Court, Coalmont, Hawaii, 1071",
+    "phone": "+1 (872) 593-3962",
+    "email": "darlene.spears@pulze.io",
+    "website": "https://pulze.ca",
+    "company": "PULZE",
+    "name": {
+      "last": "Spears",
+      "first": "Darlene"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "hQ53I4hRbi": {
+    "billing_info": {
+      "address": "447 Nolans Lane, Shepardsville, South Dakota, 5189",
+      "phone": "+1 (819) 407-3460",
+      "email": "kathrine.hawkins@undefined.biz",
+      "liaison": {
+        "last": "Hawkins",
+        "first": "Kathrine"
+      }
+    },
+    "address": "642 Dumont Avenue, Ola, Tennessee, 3893",
+    "phone": "+1 (886) 526-3205",
+    "email": "mills.wheeler@strozen.tv",
+    "website": "https://strozen.me",
+    "company": "STROZEN",
+    "name": {
+      "last": "Wheeler",
+      "first": "Mills"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "3LMCHMkV9D": {
+    "billing_info": {
+      "address": "212 Calyer Street, Hasty, Nebraska, 3392",
+      "phone": "+1 (970) 486-3835",
+      "email": "augusta.decker@undefined.net",
+      "liaison": {
+        "last": "Decker",
+        "first": "Augusta"
+      }
+    },
+    "address": "889 Herkimer Street, Soham, Colorado, 8610",
+    "phone": "+1 (929) 524-2192",
+    "email": "jo.michael@comtract.co.uk",
+    "website": "https://comtract.info",
+    "company": "COMTRACT",
+    "name": {
+      "last": "Michael",
+      "first": "Jo"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "n4TeHxV7L8": {
+    "billing_info": {
+      "address": "702 Seeley Street, Chamberino, Texas, 6270",
+      "phone": "+1 (814) 478-2799",
+      "email": "schmidt.kelly@undefined.org",
+      "liaison": {
+        "last": "Kelly",
+        "first": "Schmidt"
+      }
+    },
+    "address": "129 Micieli Place, Sultana, Iowa, 9610",
+    "phone": "+1 (951) 576-2606",
+    "email": "maryann.collins@quarmony.us",
+    "website": "https://quarmony.biz",
+    "company": "QUARMONY",
+    "name": {
+      "last": "Collins",
+      "first": "Maryann"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "n9B31fdsoR": {
+    "billing_info": {
+      "address": "696 Hamilton Avenue, Carrsville, New York, 7023",
+      "phone": "+1 (929) 589-2164",
+      "email": "lou.brewer@undefined.name",
+      "liaison": {
+        "last": "Brewer",
+        "first": "Lou"
+      }
+    },
+    "address": "492 Lawn Court, Barrelville, Alaska, 6691",
+    "phone": "+1 (986) 403-3831",
+    "email": "chrystal.jacobson@flum.io",
+    "website": "https://flum.ca",
+    "company": "FLUM",
+    "name": {
+      "last": "Jacobson",
+      "first": "Chrystal"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "qSrrgVTyNg": {
+    "billing_info": {
+      "address": "647 Bouck Court, Thomasville, Virginia, 4367",
+      "phone": "+1 (832) 508-3886",
+      "email": "benita.kinney@undefined.biz",
+      "liaison": {
+        "last": "Kinney",
+        "first": "Benita"
+      }
+    },
+    "address": "424 Doone Court, Woodburn, Guam, 4714",
+    "phone": "+1 (972) 422-3812",
+    "email": "ronda.powell@cinesanct.tv",
+    "website": "https://cinesanct.me",
+    "company": "CINESANCT",
+    "name": {
+      "last": "Powell",
+      "first": "Ronda"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "Ob3cq6qPWz": {
+    "billing_info": {
+      "address": "730 Aberdeen Street, Marion, Montana, 3941",
+      "phone": "+1 (833) 518-3504",
+      "email": "sheri.howard@undefined.net",
+      "liaison": {
+        "last": "Howard",
+        "first": "Sheri"
+      }
+    },
+    "address": "414 Fiske Place, Bayview, Washington, 7284",
+    "phone": "+1 (869) 404-2595",
+    "email": "tamera.larsen@tropolis.co.uk",
+    "website": "https://tropolis.info",
+    "company": "TROPOLIS",
+    "name": {
+      "last": "Larsen",
+      "first": "Tamera"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "uubPYJBi9y": {
+    "billing_info": {
+      "address": "417 Goodwin Place, Eagletown, Utah, 9557",
+      "phone": "+1 (810) 564-3237",
+      "email": "janette.hewitt@undefined.org",
+      "liaison": {
+        "last": "Hewitt",
+        "first": "Janette"
+      }
+    },
+    "address": "698 Horace Court, Ona, Puerto Rico, 1465",
+    "phone": "+1 (932) 490-2998",
+    "email": "casey.velazquez@emergent.us",
+    "website": "https://emergent.biz",
+    "company": "EMERGENT",
+    "name": {
+      "last": "Velazquez",
+      "first": "Casey"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "EqYZF1apDE": {
+    "billing_info": {
+      "address": "306 Aurelia Court, Onton, Federated States Of Micronesia, 4412",
+      "phone": "+1 (984) 449-3807",
+      "email": "shelia.mcbride@undefined.name",
+      "liaison": {
+        "last": "Mcbride",
+        "first": "Shelia"
+      }
+    },
+    "address": "999 Bulwer Place, Basye, West Virginia, 9782",
+    "phone": "+1 (862) 514-2112",
+    "email": "rosales.henry@urbanshee.io",
+    "website": "https://urbanshee.ca",
+    "company": "URBANSHEE",
+    "name": {
+      "last": "Henry",
+      "first": "Rosales"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "8H5eYQsqke": {
+    "billing_info": {
+      "address": "154 Llama Court, Weedville, Delaware, 6533",
+      "phone": "+1 (924) 467-2466",
+      "email": "castillo.galloway@undefined.biz",
+      "liaison": {
+        "last": "Galloway",
+        "first": "Castillo"
+      }
+    },
+    "address": "910 Engert Avenue, Floriston, Florida, 5131",
+    "phone": "+1 (938) 435-3771",
+    "email": "kinney.compton@nixelt.tv",
+    "website": "https://nixelt.me",
+    "company": "NIXELT",
+    "name": {
+      "last": "Compton",
+      "first": "Kinney"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "nw1ufbquab": {
+    "billing_info": {
+      "address": "706 Vandervoort Place, Murillo, Arizona, 7116",
+      "phone": "+1 (932) 410-2559",
+      "email": "elliott.trujillo@undefined.net",
+      "liaison": {
+        "last": "Trujillo",
+        "first": "Elliott"
+      }
+    },
+    "address": "763 Brightwater Court, Bergoo, Idaho, 4898",
+    "phone": "+1 (884) 498-2098",
+    "email": "knox.bird@marqet.co.uk",
+    "website": "https://marqet.info",
+    "company": "MARQET",
+    "name": {
+      "last": "Bird",
+      "first": "Knox"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "mUzTwZZ8DO": {
+    "billing_info": {
+      "address": "320 Ivan Court, Beaulieu, Nevada, 7837",
+      "phone": "+1 (965) 512-2589",
+      "email": "evelyn.oneill@undefined.org",
+      "liaison": {
+        "last": "Oneill",
+        "first": "Evelyn"
+      }
+    },
+    "address": "354 Grafton Street, Whitewater, Palau, 7401",
+    "phone": "+1 (898) 569-2251",
+    "email": "wendi.mcfadden@lumbrex.us",
+    "website": "https://lumbrex.biz",
+    "company": "LUMBREX",
+    "name": {
+      "last": "Mcfadden",
+      "first": "Wendi"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "87d3N6jVf6": {
+    "billing_info": {
+      "address": "933 Hubbard Place, Independence, Kansas, 8123",
+      "phone": "+1 (827) 536-3453",
+      "email": "fitzpatrick.griffith@undefined.name",
+      "liaison": {
+        "last": "Griffith",
+        "first": "Fitzpatrick"
+      }
+    },
+    "address": "105 Onderdonk Avenue, Fillmore, District Of Columbia, 2833",
+    "phone": "+1 (881) 525-3837",
+    "email": "huff.perry@talkola.io",
+    "website": "https://talkola.ca",
+    "company": "TALKOLA",
+    "name": {
+      "last": "Perry",
+      "first": "Huff"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "YerhdGREaU": {
+    "billing_info": {
+      "address": "372 Leonora Court, Jackpot, California, 9263",
+      "phone": "+1 (925) 555-3395",
+      "email": "cervantes.watkins@undefined.biz",
+      "liaison": {
+        "last": "Watkins",
+        "first": "Cervantes"
+      }
+    },
+    "address": "251 Colin Place, Osmond, South Carolina, 1042",
+    "phone": "+1 (829) 441-2927",
+    "email": "mcmahon.harris@biospan.tv",
+    "website": "https://biospan.me",
+    "company": "BIOSPAN",
+    "name": {
+      "last": "Harris",
+      "first": "Mcmahon"
+    },
+    "picture": "http://placehold.it/32x32"
+  },
+  "PnuYuDu08q": {
+    "billing_info": {
+      "address": "259 Duffield Street, Bennett, Marshall Islands, 9979",
+      "phone": "+1 (902) 410-2329",
+      "email": "ola.barlow@undefined.net",
+      "liaison": {
+        "last": "Barlow",
+        "first": "Ola"
+      }
+    },
+    "address": "957 Fulton Street, Santel, Massachusetts, 6620",
+    "phone": "+1 (842) 543-2706",
+    "email": "saunders.huber@poshome.co.uk",
+    "website": "https://poshome.info",
+    "company": "POSHOME",
+    "name": {
+      "last": "Huber",
+      "first": "Saunders"
+    },
+    "picture": "http://placehold.it/32x32"
+  }
+}
