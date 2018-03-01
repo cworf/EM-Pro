7032af9a9793d48063252725eafeb8fd00cef293
cworf <colin.worf@gmail.com>
2018-02-28 22:34:19 -0800

    finish venues list

--- src/components/ui/VenuePanel.js
+++ src/components/ui/VenuePanel.js
@@ -12,14 +12,14 @@ import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 import Chip from 'material-ui/Chip';
 import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
-import Avatar from 'material-ui/Avatar';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
+    marginBottom: '20px',
   },
   heading: {
-    fontSize: theme.typography.pxToRem(15),
+    fontSize: theme.typography.pxToRem(18),
   },
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
@@ -31,14 +31,20 @@ const styles = theme => ({
     width: 20,
   },
   details: {
-    alignItems: 'center',
+    alignItems: 'flex-start',
   },
   column: {
     flexBasis: '33.33%',
   },
+  hasChildren: {
+    display:'flex',
+  },
+  innerColumn: {
+    flexBasis: '50%',
+  },
   helper: {
     borderLeft: `2px solid ${theme.palette.divider}`,
-    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+    padding: `0px ${theme.spacing.unit * 2}px`,
   },
   link: {
     color: theme.palette.primary.main,
@@ -58,35 +64,51 @@ function handleClick() {
 
 function Venues(props) {
   const { classes } = props;
-  const { physical_address, contact_info, stages } = props.venue;
+  const { contact_info, stages, name } = props.venue;
   const { street, street2, city, state, zip } = contact_info.mailing_address;
+  const br = document.createElement('br');
   return (
     <div className={classes.root}>
       <ExpansionPanel defaultExpanded>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
           <div className={classes.column}>
-            <Typography className={classes.heading}>Wilma</Typography>
+            <Typography className={classes.heading}>{name}</Typography>
           </div>
           <div className={classes.column}>
-            <Typography className={classes.secondaryHeading}>Missoula, MT</Typography>
+            <Typography className={classes.secondaryHeading}>{city}, {state}</Typography>
+          </div>
+          <div className={classes.column}>
+            <Typography className={classes.secondaryHeading}>{Object.keys(stages).length} {Object.keys(stages).length === 1 ? 'Stage' : 'Stages'}</Typography>
           </div>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails className={classes.details}>
           <div className={classes.column}>
-            <Typography variant="subheading">
+            <Typography style={{textDecoration: 'underline', marginBottom: '10px'}} variant="subheading">
               Contact Info
             </Typography>
             <Typography variant="body1">
-              Liaison: {contact_info.liaison}
-              <br/>
-              Phone Numbers:
-              <br />
+              <strong>Liaison:</strong>
+              <div className={classes.helper}>
+                {contact_info.liaison}
+              </div>
+              <strong>Phone Numbers:</strong>
               {Object.keys(contact_info.phoneNumbers).map(type =>
-                `${type}: ${contact_info.phoneNumbers[type]}`
+                <Typography className={classes.helper} variant="caption">
+                  <strong>{type}:</strong> {contact_info.phoneNumbers[type]}
+                </Typography>
               )}
-              Email: {contact_info.email}
-              <br />
-              Mailing Address: {street} {city}, {state} {zip}
+              <strong>Emails:</strong>
+                {contact_info.emails.map(email =>
+                  <div className={classes.helper}>
+                    {email}
+                  </div>
+                )}
+              <strong>Mailing Address:</strong>
+                <div className={classes.helper}>
+                  {street } <br />
+                {street2 ? street2 : null}{street2 ? <br /> : null }
+                  {city}, {state} {zip}
+                </div>
             </Typography>
           </div>
           <div className={classes.column}>
@@ -96,12 +118,14 @@ function Venues(props) {
             <Typography variant="subheading">
               Stages
             </Typography>
-            <Chip
-        avatar={<Avatar>MB</Avatar>}
-        label="Clickable Chip"
-        onClick={handleClick}
-        className={classes.chip}
-      />
+
+            {Object.keys(stages).map(stageId =>
+              <Chip
+                label={stages[stageId].name}
+                onClick={handleClick}
+                className={classes.chip}
+              />
+            )}
           </div>
         </ExpansionPanelDetails>
         <Divider />
