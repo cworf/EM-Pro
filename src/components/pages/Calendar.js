2c43b041f1b6a31303a1139781ec3cff9572a525
cworf <colin.worf@gmail.com>
2018-03-02 12:43:25 -0800

    create fullscreen dialog on event click

--- src/components/pages/Calendar.js
+++ src/components/pages/Calendar.js
@@ -4,31 +4,80 @@ import './Calendar.css'
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import events from '../../assets/data/events.js';
+import { withStyles } from 'material-ui/styles';
+import PropTypes from 'prop-types';
+import Dialog from 'material-ui/Dialog';
+import Slide from 'material-ui/transitions/Slide';
+
+import EventDetail from '../ui/EventDetail'
+
+const styles = {
+  appBar: {
+    position: 'relative',
+  },
+  flex: {
+    flex: 1,
+  },
+};
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
 
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
-function Calendar(props){
 
-  return (
-    <div style={{height: '80vh'}}>
-      <BigCalendar
-        selectable
-        events={events}
-        defaultView="month"
-        scrollToTime={new Date(1970, 1, 1, 6)}
-        defaultDate={new Date(2015, 3, 12)}
-        onSelectEvent={event => alert(event.title)}
-        onSelectSlot={slotInfo =>
-          alert(
-            `selected slot:
-            start ${slotInfo.start.toLocaleString()}
-            end: ${slotInfo.end.toLocaleString()}
-            action: ${slotInfo.action}`
-          )
-        }
-      />
-    </div>
-  );
+
+class Calendar extends React.Component{
+
+  state = {
+    open: false,
+    clickedEvent: null,
+  };
+
+  handleClickOpen = (event) => {
+    this.setState({ open: true, clickedEvent: event });
+  };
+
+  handleClose = () => {
+    this.setState({ ...this.state, open: false });
+  };
+
+  render(){
+    return (
+      <div style={{height: '80vh'}}>
+        <BigCalendar
+          selectable
+          events={events}
+          defaultView="month"
+          scrollToTime={new Date(1970, 1, 1, 6)}
+          defaultDate={new Date(2015, 3, 12)}
+          onSelectEvent={event => this.handleClickOpen(event)}
+          onSelectSlot={slotInfo =>
+            alert(
+              `selected slot:
+              start ${slotInfo.start.toLocaleString()}
+              end: ${slotInfo.end.toLocaleString()}
+              action: ${slotInfo.action}`
+            )
+
+          }
+        />
+        <Dialog
+          fullScreen
+          open={this.state.open}
+          onClose={this.handleClose}
+          transition={Transition}
+        >
+          <EventDetail onClickClose={this.handleClose} event={this.state.clickedEvent} />
+        </Dialog>
+      </div>
+    );
+  }
 }
 
-export default Calendar;
+Calendar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Calendar);
