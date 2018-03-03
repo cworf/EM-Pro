dfc47b13a4584d96d27adf16e395f7e37fb42ae0
cworf <colin.worf@gmail.com>
2018-03-02 16:39:46 -0800

    move event detail into pages tier

---
+++ src/components/pages/EventDetail.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import CloseIcon from 'material-ui-icons/Close';
+import SwipeableViews from 'react-swipeable-views';
+import Tabs, { Tab } from 'material-ui/Tabs';
+
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+  },
+  appBar: {
+    position: 'relative',
+  },
+  flex: {
+    flex: 1,
+  },
+});
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+  dir: PropTypes.string.isRequired,
+};
+
+function TabContainer({ children, dir }) {
+  return (
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
+}
+
+class EventDetail extends React.Component {
+
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+
+  render() {
+    const { classes, theme, event } = this.props;
+
+    return (
+      <div>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              {event.title}
+            </Typography>
+            <IconButton color="inherit" onClick={this.props.onClickClose} aria-label="Close">
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.root}>
+          <AppBar position="static" color="default">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              fullWidth
+            >
+              <Tab label="Item One" />
+              <Tab label="Item Two" />
+              <Tab label="Item Three" />
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={this.state.value}
+            onChangeIndex={this.handleChangeIndex}
+          >
+            <TabContainer dir={theme.direction}>Item One</TabContainer>
+            <TabContainer dir={theme.direction}>Item Two</TabContainer>
+            <TabContainer dir={theme.direction}>Item Three</TabContainer>
+          </SwipeableViews>
+        </div>
+      </div>
+    );
+  }
+}
+
+EventDetail.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onClickClose: PropTypes.func.isRequired,
+  event: PropTypes.object,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(EventDetail);
