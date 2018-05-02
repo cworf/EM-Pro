5bdf68725d4b63011425bd5755bf7fc576634a50
cworf <colin.worf@gmail.com>
2018-05-02 13:32:26 -0700

    prep venueDetail component, base structure

--- src/components/pages/VenueDetail.js
+++ src/components/pages/VenueDetail.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {Document, Collection} from 'firestorter';
+import {observer} from 'mobx-react';
+import Typography from 'material-ui/Typography';
+import Divider from 'material-ui/Divider';
 
-function VenueDetail(props){
 
-  return (
-    <div>
-      <h1>Venue Detail</h1>
-    </div>
-  );
-}
+
+const VenueDetail = observer(class VenueDetail extends React.Component {
+
+  venue = new Document(`venues/${this.props.match.params.id}`)
+  stages = new Collection(`venues/${this.props.match.params.id}/stages`)
+
+  render(){
+    return (
+      <div>
+        <Typography variant="display1" gutterBottom>
+          {this.venue.data.name}
+        </Typography>
+        <Divider />
+      </div>
+    );
+  }
+})
 
 export default VenueDetail;
