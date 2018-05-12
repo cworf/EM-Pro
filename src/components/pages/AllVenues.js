050e0d437951fedc4d21225e4c6a1b258f0c0b71
cworf <colin.worf@gmail.com>
2018-05-12 00:02:26 -0700

    connect venues to data and auth

--- src/components/pages/AllVenues.js
+++ src/components/pages/AllVenues.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { inject, observer } from 'mobx-react';
+import { compose } from 'recompose';
+import withAuthorization from '../Session/withAuthorization';
+import withData from '../Session/withData';
 import VenuePanel from '../ui/VenuePanel';
 // import venues from '../../assets/data/venues';
-import {venues} from '../appStore'
-import {observer} from 'mobx-react';
 
-const AllVenues = observer(function AllVenues(props){
+function AllVenues({dataStore: {venues}}){
 
   return (
     <div>
       {venues.docs.map(venue =>
         <VenuePanel key={venue.id} venue={venue} />)}
-
     </div>
   );
-});
+};
 
-export default AllVenues;
+const authCondition = (authUser) => !!authUser;
+
+export default compose(
+  withAuthorization(authCondition),
+  withData(['/venues']),
+  inject('dataStore'),
+  observer
+)(AllVenues);
