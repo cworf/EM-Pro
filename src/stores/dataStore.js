5089d677e77ebd135e554534acf585c1991b6f32
cworf <colin.worf@gmail.com>
2018-05-11 19:59:31 -0700

    construct base firestore collection in data store

--- src/stores/dataStore.js
+++ src/stores/dataStore.js
@@ -1,10 +1,17 @@
 import { action } from 'mobx';
-import { Collection } from 'firestorter'
+import { Collection, Document  } from 'firestorter'
 
 class DataStore {
   constructor(rootStore) {
     this.rootStore = rootStore;
     this.eventsCol = new Collection()
+    this.clients = new Collection();
+    this.venues = new Collection();
+    this.inventory = new Collection();
+    this.orders = new Collection();
+    this.conflicts = new Collection();
+    this.users = new Collection();
+    this.company = new Document();
   }
 
   @action setData = path => {
