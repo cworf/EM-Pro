314b9625f0acda075c0674fa649a9a4e26067540
cworf <colin.worf@gmail.com>
2018-05-16 21:03:27 -0700

    style inventory section

--- src/components/pages/Inventory.js
+++ src/components/pages/Inventory.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import AllInventory from './AllInventory'
 import InventoryDetail from './InventoryDetail'
+import Paper from 'material-ui/Paper';
 import { Switch, Route } from 'react-router-dom';
 
 function Inventory(props){
 
   return (
-    <Switch>
-      <Route exact path='/inventory' component={AllInventory} />
-      <Route path='/inventory/:id' component={InventoryDetail} />
-    </Switch>
+    <Paper style={{maxWidth:950,
+      padding: 20,
+      background: '#2d323a',
+      margin: 'auto',}}>
+      <Switch>
+        <Route exact path='/inventory' component={AllInventory} />
+        <Route path='/inventory/:id' component={InventoryDetail} />
+      </Switch>
+    </Paper>
   );
 }
 
